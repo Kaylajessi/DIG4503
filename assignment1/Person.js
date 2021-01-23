e465d627970b5cdc1dad4d8d5c5ea0e800976d06
Kaylajessi <robertsjkayla@knights.ucf.edu>
2021-01-22 22:34:51 -0500

    Lab1

---
+++ assignment1/Person.js
@@ -0,0 +1,39 @@
+/*Create a Person class (in a file named "Person.js") with the following:
+Properties:
+name (String)
+favoriteColor (String)
+Methods:
+constructor()
+Should accept two arguments and store their values as part of the class
+speak()
+This function should use console.log() to show the person's name in their favorite color*/
+
+class Person {
+    constructor() {
+      this.name = "Kayla";
+      this.favoritecolor = "purple";
+    }
+  
+    speak() {
+      console.log(name, favoritecolor);
+    }
+  }
+
+  const c = new Kayla();
+
+/*Create a class using your first name (for example, mine would be "Dan.js") with the following:
+Should extend the Person class
+Should use super() in its own constructor() to pass the arguments to what it is extending
+Should import the "Person.js" file.*/
+
+  class Kayla {
+
+    name = "Kayla";
+    favoritecolor = "purple";
+  
+    constructor(name, favoritecolor) {
+      this.name = "Kayla";
+      this.favoritecolor = "purple";
+    }
+  
+  }
