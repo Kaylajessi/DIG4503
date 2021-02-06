6bc02840d6fbbcc0d1bbb21f24fda74c3a70a736
Kaylajessi <robertsjkayla@knights.ucf.edu>
2021-02-06 14:23:47 -0500

    lab3

---
+++ lab3/homePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render () {
+    return (
+      <p>firstName</p>
+    );
+  }
+}
+
+export default HomePage;
---
+++ lab3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from "./homePage.js";
+
+function Name() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
